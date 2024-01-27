38e4a6e3ba3242fc012a0b4f6102d46f063ac71d
Haseab <hullah3000@gmail.com>
2024-01-27 14:34:05 -0500

    small fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,13 @@ import AreaGraph from "@/components/area";
  */
 import BarGraph from "@/components/bar";
 import MetricComponent from "@/components/metric";
-import { useEffect, useState } from "react";
+import { Title } from "@tremor/react";
+import { useEffect, useRef, useState } from "react";
 import { BarData, ChartData } from "./constant";
 import { roundToThree } from "./utils";
+
+const refreshTime = 30;
+const pollingInterval = 1000;
 
 const targets = {
   hours_free: "80.5",
@@ -46,6 +50,8 @@ export default function Component() {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(refreshTime);
+  const timeLeftRef = useRef(0);
   const [distractionData, setDistractionData] = useState<BarData[]>(
     weekdays.map((day) => ({
       date: day,
@@ -84,178 +90,178 @@ export default function Component() {
     }))
   );
 
-  // const pollingInterval = 300_000; // 5 minutes
-  const pollingInterval = 30_000; // 0.5 minutes
+  const fetchData = async () => {
+    console.log("fetching data ...");
+    setIsLoading(true);
+    try {
+      const response = await fetch("http://localhost:3002/metrics");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+      const data = result.data;
+      const {
+        adhocTimeList,
+        oneHUTList,
+        p1HUTList,
+        n1HUTList,
+        nw1HUTList,
+        w1HUTList,
+        hoursFreeList,
+        distractionCountList,
+        // efficiencyList,
+      } = data as MetricsResponse;
+
+      const sumValues = (obj: Record<string, number>) =>
+        Object.values(obj).reduce((a, b) => a + b, 0);
+
+      const p1HUT = sumValues(p1HUTList);
+      const n1HUT = sumValues(n1HUTList);
+      const nw1HUT = sumValues(nw1HUTList);
+      const w1HUT = sumValues(w1HUTList);
+      const hours_free = sumValues(hoursFreeList);
+      const distraction_count = sumValues(distractionCountList);
+      const adhoc_time = sumValues(adhocTimeList);
+
+      console.log(data);
+
+      const newDistractionData = distractionData.map((item, index) => {
+        const distractionCountValue =
+          Object.values(distractionCountList)[index];
+        return {
+          ...item,
+          value: distractionCountValue || item["value"],
+        };
+      });
+
+      setDistractionData(newDistractionData);
+
+      const newBarData = barData.map((item, index) => {
+        const adhocTimeValue = Object.values(adhocTimeList)[index];
+        return {
+          ...item,
+          value: adhocTimeValue || item["value"],
+        };
+      });
+
+      setBarData(newBarData);
+      console.log(newBarData);
+
+      const newChartData = chartData.map((item, index) => {
+        const p1HUTValue = Object.values(p1HUTList)[index];
+        const oneHUTValue = Object.values(oneHUTList)[index];
+        return {
+          ...item,
+          p1HUT: p1HUTValue || item.p1HUT,
+          oneHUT: oneHUTValue || item.oneHUT,
+        };
+      });
+
+      setChartData(newChartData);
+
+      const newMetricsData = [
+        {
+          metric: "Hours Free",
+          score: hours_free,
+          percentageOfTarget: roundToThree(
+            Math.min((hours_free / parseFloat(targets.hours_free)) * 100, 100)
+          ),
+          targetScore: targets.hours_free,
+        },
+        {
+          metric: "1HUT Time",
+          score: p1HUT + n1HUT + nw1HUT + w1HUT,
+          percentageOfTarget: roundToThree(
+            Math.min(
+              ((p1HUT + n1HUT + nw1HUT + w1HUT) / parseFloat(targets.oneHUT)) *
+                100,
+              100
+            )
+          ),
+          targetScore: targets.oneHUT,
+        },
+        {
+          metric: "Ad Hoc Time",
+          score: adhoc_time,
+          percentageOfTarget: roundToThree(
+            Math.min(
+              ((parseFloat(targets.hours_free) - adhoc_time) /
+                parseFloat(targets.hours_free)) *
+                100,
+              100
+            )
+          ),
+          targetScore: targets.adhoc_time,
+        },
+        {
+          metric: "Distraction #",
+          score: distraction_count,
+          percentageOfTarget: roundToThree(
+            Math.min(
+              (distraction_count / parseFloat(targets.distraction_count)) * 100,
+              100
+            )
+          ),
+          targetScore: targets.distraction_count,
+        },
+        {
+          metric: "p1HUT Time",
+          score: p1HUT,
+          percentageOfTarget: roundToThree(
+            Math.min((p1HUT / parseFloat(targets.p1HUT)) * 100, 100)
+          ),
+          targetScore: targets.p1HUT,
+        },
+        {
+          metric: "n1HUT",
+          score: n1HUT,
+          percentageOfTarget: roundToThree(
+            Math.min((n1HUT / parseFloat(targets.n1HUT)) * 100, 100)
+          ),
+          targetScore: targets.n1HUT,
+        },
+        {
+          metric: "nw1HUT",
+          score: nw1HUT,
+          percentageOfTarget: roundToThree(
+            Math.min((nw1HUT / parseFloat(targets.nw1HUT)) * 100, 100)
+          ),
+          targetScore: targets.nw1HUT,
+        },
+        {
+          metric: "w1HUT",
+          score: w1HUT,
+          percentageOfTarget: roundToThree(
+            Math.min((w1HUT / parseFloat(targets.w1HUT)) * 100, 100)
+          ),
+          targetScore: targets.w1HUT,
+        },
+      ];
+
+      setMetricsData(newMetricsData);
+
+      setData(result);
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    let isMounted = true; // Flag to handle component mount state
-
-    const fetchData = async () => {
-      if (!isMounted) return; // Avoid state updates if the component is not mounted
-      setIsLoading(true);
-      try {
-        const response = await fetch("http://localhost:3002/metrics");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const result = await response.json();
-        const data = result.data;
-        const {
-          adhocTimeList,
-          oneHUTList,
-          p1HUTList,
-          n1HUTList,
-          nw1HUTList,
-          w1HUTList,
-          hoursFreeList,
-          distractionCountList,
-          // efficiencyList,
-        } = data as MetricsResponse;
-
-        const sumValues = (obj: Record<string, number>) =>
-          Object.values(obj).reduce((a, b) => a + b, 0);
-
-        const p1HUT = sumValues(p1HUTList);
-        const n1HUT = sumValues(n1HUTList);
-        const nw1HUT = sumValues(nw1HUTList);
-        const w1HUT = sumValues(w1HUTList);
-        const hours_free = sumValues(hoursFreeList);
-        const distraction_count = sumValues(distractionCountList);
-        const adhoc_time = sumValues(adhocTimeList);
-
-        console.log(data);
-
-        const newDistractionData = distractionData.map((item, index) => {
-          const distractionCountValue =
-            Object.values(distractionCountList)[index];
-          return {
-            ...item,
-            value: distractionCountValue || item["value"],
-          };
-        });
-
-        setDistractionData(newDistractionData);
-
-        const newBarData = barData.map((item, index) => {
-          const adhocTimeValue = Object.values(adhocTimeList)[index];
-          return {
-            ...item,
-            value: adhocTimeValue || item["value"],
-          };
-        });
-
-        setBarData(newBarData);
-        console.log(newBarData);
-
-        const newChartData = chartData.map((item, index) => {
-          const p1HUTValue = Object.values(p1HUTList)[index];
-          const oneHUTValue = Object.values(oneHUTList)[index];
-          return {
-            ...item,
-            p1HUT: p1HUTValue || item.p1HUT,
-            oneHUT: oneHUTValue || item.oneHUT,
-          };
-        });
-
-        setChartData(newChartData);
-
-        const newMetricsData = [
-          {
-            metric: "Hours Free",
-            score: hours_free,
-            percentageOfTarget: roundToThree(
-              Math.min((hours_free / parseFloat(targets.hours_free)) * 100, 100)
-            ),
-            targetScore: targets.hours_free,
-          },
-          {
-            metric: "1HUT Time",
-            score: p1HUT + n1HUT + nw1HUT + w1HUT,
-            percentageOfTarget: roundToThree(
-              Math.min(
-                ((p1HUT + n1HUT + nw1HUT + w1HUT) /
-                  parseFloat(targets.oneHUT)) *
-                  100,
-                100
-              )
-            ),
-            targetScore: targets.oneHUT,
-          },
-          {
-            metric: "Ad Hoc Time",
-            score: adhoc_time,
-            percentageOfTarget: roundToThree(
-              Math.min(
-                ((parseFloat(targets.hours_free) - adhoc_time) /
-                  parseFloat(targets.hours_free)) *
-                  100,
-                100
-              )
-            ),
-            targetScore: targets.adhoc_time,
-          },
-          {
-            metric: "Distraction #",
-            score: distraction_count,
-            percentageOfTarget: roundToThree(
-              Math.min(
-                (distraction_count / parseFloat(targets.distraction_count)) *
-                  100,
-                100
-              )
-            ),
-            targetScore: targets.distraction_count,
-          },
-          {
-            metric: "p1HUT Time",
-            score: p1HUT,
-            percentageOfTarget: roundToThree(
-              Math.min((p1HUT / parseFloat(targets.p1HUT)) * 100, 100)
-            ),
-            targetScore: targets.p1HUT,
-          },
-          {
-            metric: "n1HUT",
-            score: n1HUT,
-            percentageOfTarget: roundToThree(
-              Math.min((n1HUT / parseFloat(targets.n1HUT)) * 100, 100)
-            ),
-            targetScore: targets.n1HUT,
-          },
-          {
-            metric: "nw1HUT",
-            score: nw1HUT,
-            percentageOfTarget: roundToThree(
-              Math.min((nw1HUT / parseFloat(targets.nw1HUT)) * 100, 100)
-            ),
-            targetScore: targets.nw1HUT,
-          },
-          {
-            metric: "w1HUT",
-            score: w1HUT,
-            percentageOfTarget: roundToThree(
-              Math.min((w1HUT / parseFloat(targets.w1HUT)) * 100, 100)
-            ),
-            targetScore: targets.w1HUT,
-          },
-        ];
-
-        setMetricsData(newMetricsData);
-
-        setData(result);
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
     fetchData(); // Fetch data immediately on component mount
-
-    const interval = setInterval(fetchData, pollingInterval); // Set up polling
+    const interval = setInterval(() => {
+      timeLeftRef.current += 1;
+      setTimeLeft(timeLeftRef.current);
+      // console.log(`${timeLeftRef.current} second passed`);
+      if (timeLeftRef.current === refreshTime) {
+        fetchData();
+        timeLeftRef.current = 0;
+        setTimeLeft(timeLeftRef.current);
+      }
+    }, pollingInterval); // Set up polling
 
     return () => {
-      isMounted = false; // Set the flag to false when component unmounts
       clearInterval(interval); // Clear the interval
     };
   }, []);
@@ -309,7 +315,10 @@ export default function Component() {
         </header>
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 dark:bg-gray-900">
           <div className="container mx-auto px-6 py-8">
-            <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+            <Title className="grid gap-6 mb-8 text-center">
+              Refreshing in {refreshTime - timeLeftRef.current} seconds
+            </Title>
+            <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4 mt-5">
               {metricsData.map((data, index) => (
                 <MetricComponent
                   key={index}
