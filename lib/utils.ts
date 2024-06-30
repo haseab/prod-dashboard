9395096c7dfaf36414c5db914318ae7a745c1453
Haseab <hullah3000@gmail.com>
2024-06-30 13:13:18 -0700

    fix time to be local only to my current location

--- lib/utils.ts
+++ lib/utils.ts
@@ -24,6 +24,21 @@ export const simpleMovingAverage = (prices: number[], interval: number) => {
   return results;
 };
 
+export const formatPacificTime = (date: Date) => {
+  const options: Record<string, string | boolean> = {
+    timeZone: "America/Los_Angeles", // Pacific Time
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false, // Use 24-hour time format
+  };
+
+  return new Intl.DateTimeFormat("en-US", options).format(date);
+};
+
 const getMetricsPercentage = ({
   partialNewMetricsData,
   inverse,
