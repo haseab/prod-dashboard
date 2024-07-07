757ea0ca915910bf1649d8ecaf68620c7a86dc2a
Haseab <haseab.amin@gmail.com>
2024-07-07 04:22:16 -0700

    add comments about flow state

--- app/page.tsx
+++ app/page.tsx
@@ -422,6 +422,21 @@ export default function Component() {
                           "America/Los_Angeles"
                         )}
                       </p>
+                      {flow > 2.5 ? (
+                        <p className="text-red-500 text-sm">
+                          Currently in a flow state for over 2.5 hours
+                        </p>
+                      ) : flow > 1.5 ? (
+                        <p className="text-purple-500 text-sm">
+                          Currently in a flow state for over 1.5 hours
+                        </p>
+                      ) : flow > 0.8 ? (
+                        <p className="text-green-500 text-sm">
+                          Currently in a flow state for about 1 hour
+                        </p>
+                      ) : (
+                        <></>
+                      )}
                     </div>
                   </Card>
                 </div>
