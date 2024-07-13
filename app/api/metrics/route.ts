69d84d3b16f6c410055c47fd1797d1cf14ae2bff
Haseab <haseab.amin@gmail.com>
2024-07-13 01:38:44 -0700

    add unstable caching

--- app/api/metrics/route.ts
+++ app/api/metrics/route.ts
@@ -1,3 +1,6 @@
+import { revalidateCache } from "@/lib/utils";
+import { unstable_cache } from "next/cache";
+
 export const revalidate = 15;
 
 export async function GET() {
@@ -16,46 +19,47 @@ export async function GET() {
   }
 }
 
-const fetchTimeData = async () => {
-  try {
-    const response = await fetch(`${process.env.SERVER_URL}/metrics`);
+const fetchTimeData = unstable_cache(
+  async () => {
+    try {
+      const response = await fetch(`${process.env.SERVER_URL}/metrics`);
 
-    // Check for network errors
-    if (!response.ok) {
-      throw new Error(`Fetch error: ${response.statusText}`);
+      // Check for network errors
+      if (!response.ok) {
+        throw new Error(`Fetch error: ${response.statusText}`);
+      }
+
+      console.log("response", response);
+
+      if (!response.ok) {
+        throw new Error(`An error occurred: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+
+      return data;
+    } catch (error) {
+      console.error("FetchTimeData error:", error);
+      throw error;
     }
-
-    console.log("response", response);
-
-    if (!response.ok) {
-      throw new Error(`An error occurred: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-
-    return data;
-  } catch (error) {
-    console.error("FetchTimeData error:", error);
-    throw error;
+  },
+  ["time"],
+  {
+    tags: ["time"],
   }
-};
-//   ["time"],
-//   {
-//     tags: ["time"],
-//   }
-// );
+);
 
-// // Function to revalidate the 'time' tag
-// function startRevalidating() {
-//   setInterval(async () => {
-//     try {
-//       await revalidateCache(["time"]);
-//       console.log("Revalidated time tag");
-//     } catch (error) {
-//       console.error("Error revalidating time tag", error);
-//     }
-//   }, 15000); // Revalidate every 30 seconds
-// }
+// Function to revalidate the 'time' tag
+function startRevalidating() {
+  setInterval(async () => {
+    try {
+      await revalidateCache(["time"]);
+      console.log("Revalidated time tag");
+    } catch (error) {
+      console.error("Error revalidating time tag", error);
+    }
+  }, 15000); // Revalidate every 30 seconds
+}
 
-// // Start the revalidation process
-// startRevalidating();
+// Start the revalidation process
+startRevalidating();
