de522428770165eba171dc56aa8cc6fc482786b1
Haseab <haseab.amin@gmail.com>
2024-07-23 00:02:18 -0700

    update historical metrics

--- app/constant.ts
+++ app/constant.ts
@@ -125,7 +125,7 @@ export const weeklyProductiveFlow: HistoricalWeeklyData[] = [
   { week: 77, date: "06-17", productiveFlow: 46.202, flowPercentage: 0.53428 },
   { week: 78, date: "06-24", productiveFlow: 53.496, flowPercentage: 0.59195 },
   { week: 79, date: "07-01", productiveFlow: 60.421, flowPercentage: 0.63102 },
-  { week: 80, date: "07-08", productiveFlow: 51.102, flowPercentage: 0.5486 },
+  { week: 80, date: "07-08", productiveFlow: 51.826, flowPercentage: 0.57138 },
   { week: 81, date: "07-15", productiveFlow: 51.841, flowPercentage: 0.57138 },
 ];
 
