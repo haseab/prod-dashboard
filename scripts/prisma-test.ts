0a0b71898eb58942dfdb873dca93fe30d77767dc
Haseab <haseab.amin@gmail.com>
2024-09-26 05:23:28 -0400

    change local script slightly

--- scripts/prisma-test.ts
+++ scripts/prisma-test.ts
@@ -1,18 +1,22 @@
 import prisma from "@/app/lib/prisma";
 
 const main = async () => {
+  // const random = Math.random() * 100;
+  const random = 50;
+
   console.log(
     "ABOUT TO UPDATE THE DB! WITH THE FOLLOWING PILE NUMBER: ",
-    65.75
+    random
   );
 
   try {
     await prisma.pile_history.create({
       data: {
-        amount: 65.75,
+        amount: random,
         createdAt: new Date(),
       },
     });
+
     console.log("Record successfully created");
   } catch (e) {
     console.error("Error occurred during database operation:", e);
