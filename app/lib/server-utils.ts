1cd806736489eeab435669009d0296fac9dfb0a0
Haseab <haseab.amin@gmail.com>
2024-11-03 12:14:59 -0500

    add taskdetails fetching

--- app/lib/server-utils.ts
+++ app/lib/server-utils.ts
@@ -1,13 +1,17 @@
 import prisma from "./prisma";
 
-export const updateBacklogToDb = async (num: number) => {
+export const updateBacklogToDb = async (
+  taskBacklog: number,
+  taskBacklogDetails: Record<string, string | number>[]
+) => {
   console.log(
     "ABOUT TO UPDATE THE DB! WITH THE FOLLOWING TASK BACKLOG NUMBER: ",
-    num
+    taskBacklog
   );
   return await prisma.task_backlog.create({
     data: {
-      amount: num,
+      amount: taskBacklog,
+      details: taskBacklogDetails,
       createdAt: new Date(),
     },
   });
